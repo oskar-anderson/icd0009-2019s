697f690bcde1f9f434d63458f6fd61cb1de7caea
kaande <kaande@ttu.ee>
2020-02-15 22:35:38 +0200

    rebased and combined

---
+++ DistributedDemo/Domain/ContactType.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class ContactType
+    {
+        public int ContactTypeId { get; set; }
+        
+        [MaxLength(64)]
+        public string ContactTypeValue { get; set; }
+
+        public ICollection<Contact> Contacts { get; set; }
+    }
+}
---
+++ DistributedDemo/WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        /*
+        dotnet tool install --global dotnet-aspnet-codegenerator
+        dotnet tool update --global dotnet-aspnet-codegenerator
+
+        dotnet ef migrations add InitialDbCreation --project WebApp --startup-project WebApp
+        dotnet ef database update --project WebApp --startup-project WebApp
+        
+        run in webApp folder
+        dotnet aspnet-codegenerator controller -name PersonsController -actions -m Person -dc ApplicationDbContext -outDir Controllers --useDefaultLayout --useAsyncActions --referenceScriptLibraries -f
+        dotnet aspnet-codegenerator controller -name ContactsController -actions -m Contact -dc ApplicationDbContext -outDir Controllers --useDefaultLayout --useAsyncActions --referenceScriptLibraries -f
+        dotnet aspnet-codegenerator controller -name ContactTypesController -actions -m ContactType -dc ApplicationDbContext -outDir Controllers --useDefaultLayout --useAsyncActions --referenceScriptLibraries -f
+
+        dotnet tool update --global dotnet-ef
+
+        dotnet aspnet-codegenerator controller -name PersonsController -actions -m Person -dc ApplicationDbContext -outDir ApiControllers -api --useAsyncActions -f
+        dotnet aspnet-codegenerator controller -name ContactsController -actions -m Contact -dc ApplicationDbContext -outDir ApiControllers -api --useAsyncActions -f
+        dotnet aspnet-codegenerator controller -name ContactTypesController -actions -m ContactType -dc ApplicationDbContext -outDir ApiControllers -api --useAsyncActions -f
+
+        */
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<ContactType> ContactTypes { get; set; }
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
